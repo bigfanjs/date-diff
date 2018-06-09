4c05950b02b994f5f54ae3f6bb594611d7bf56fa
bigfanjs <bigfanjs@gmail.com>
2018-06-09 02:11:24 +0000

    feat: creates date picker

---
+++ src/DatePicker.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+class DatePicker extends Component {
+    handleDateChange = (type, e) => {
+        const date = e.target.value;
+
+        this.props.onDateChange({ [type]: date });
+    };
+
+    render() {
+        const { startDate, endDate } = this.state;
+
+        return (
+            <div>
+                <label htmlFor="#start-date">Start Date </label>
+                <input
+                    id="start-date"
+                    type="date"
+                    value={startDate}
+                    onChange={this.handleDateChange.bind(null, "startDate")}
+                />
+                <label htmlFor="#end-date">End Date </label>
+                <input
+                    id="end-date"
+                    type="date"
+                    value={endDate}
+                    onChange={this.handleDateChange.bind(null, "endDate")}
+                />
+            </div>
+        );
+    }
+}
+ 
+export default DatePicker;
